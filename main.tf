f3e9e0dad19aa075e641f6e525c2d1cb20c9486a
dannymaate <dannymaate@hotmail.com>
2024-04-07 17:10:36 +1000

    Finish prac05

--- main.tf
+++ main.tf
@@ -1,15 +1,27 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+terraform { 
+  required_providers { 
+    aws = { 
+      source = "hashicorp/aws" 
+      version = "~> 5.0" 
+    } 
+    docker = { 
+      source = "kreuzwerker/docker" 
+      version = "3.0.2" 
+    } 
+  } 
+} 
+ 
+provider "aws" { 
+  region = "us-east-1" 
+  shared_credentials_files = ["./credentials"] 
 }
 
-provider "aws" {
-  region                   = "us-east-1"
-  shared_credentials_files = ["./credentials"]
+provider "docker" { 
+ registry_auth { 
+   address = data.aws_ecr_authorization_token.ecr_token.proxy_endpoint 
+   username = data.aws_ecr_authorization_token.ecr_token.user_name 
+   password = data.aws_ecr_authorization_token.ecr_token.password 
+ } 
 }
 
 locals {
@@ -75,6 +87,8 @@ data "aws_subnets" "private" {
     values = [data.aws_vpc.default.id]
   }
 }
+
+data "aws_ecr_authorization_token" "ecr_token" {} 
 
 resource "aws_ecs_cluster" "taskoverflow" { 
    name = "taskoverflow" 
@@ -161,5 +175,21 @@ resource "aws_security_group" "taskoverflow" {
     cidr_blocks = ["0.0.0.0/0"] 
    } 
 }
+
+resource "aws_ecr_repository" "taskoverflow" { 
+ name = "taskoverflow" 
+}
+
+resource "docker_image" "taskoverflow" { 
+ name = "${aws_ecr_repository.taskoverflow.repository_url}:latest" 
+ build { 
+   context = "." 
+ } 
+} 
+ 
+resource "docker_registry_image" "taskoverflow" { 
+ name = docker_image.taskoverflow.name 
+}
+ 
 
 
